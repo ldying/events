20c6fac87053392feaabf6b45da534c6822e3e27
ldying <gooback@gmail.com>
2016-06-23 18:49:40 -0700

    validates future dates now

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   # before_create :validate date is today or later
 
+  validate :future_date_check
   belongs_to :user
 
   has_many :comments, dependent: :destroy
@@ -8,5 +9,14 @@ class Event < ActiveRecord::Base
   has_many :users_attendings, through: :attendings, source: :user
   validates :name, :location, :state, :date, :user, presence: true
 
+
+  private
+  def future_date_check
+    return if date.blank?
+ 
+    if date < Date.today
+    errors.add(:date, "must be today or a future date") 
+    end 
+  end
 
 end
